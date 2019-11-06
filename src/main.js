38ede9d5dbfdcbed8631e2a46e8e508b0cc46aa3
caprosset <capucinerosset@gmail.com>
2019-11-06 19:04:16 +0100

    Console.log scoreboard 2

--- src/main.js
+++ src/main.js
@@ -202,6 +202,8 @@ function main() {
         for(var i = 0; i < 10; i++) {
             var playersName = winScreen.querySelector('#name' + (i+1));
             playersName.innerHTML = scoreBoard[i].name;
+            console.log('WHAT IS SCOREBOARD');
+            console.log(scoreBoard);
             console.log(scoreBoard[i].name);
             var playersTime = winScreen.querySelector('#time' + (i+1));
             playersTime.innerHTML = scoreBoard[i].score + ' seconds';
